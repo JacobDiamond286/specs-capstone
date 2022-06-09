6c1514e702e6e65a375fbc88b04565f8f4cb0bc1
Jacob Diamond <jacobdiamond286@outlook.com>
2022-06-09 13:20:01 -0600

    creating templates

--- main.py
+++ main.py
@@ -1,70 +1,79 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from __init__ import create_app
 
-app = Flask(__name__)
-db = SQLAlchemy()
+# from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
+
+app = create_app()
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://users.sqlite3'
+# db = SQLAlchemy(app)
 
 
-#Endpoints ===========================================================
+# #Endpoints ===========================================================
 
-@app.route('/')
-def start_here():
-    return render_template('homepage.html')
+# @app.route('/')
+# def start_here():
+#     return render_template('homepage.html')
 
-@app.route('/games')
-def games_to_play():
-    return render_template('gamestoplay.html')
+# @app.route('/games')
+# def games_to_play():
+#     return render_template('gamestoplay.html')
 
-@app.route('/friends')
-def friends_games():
-    return render_template('friendsgames.html')
+# @app.route('/friends')
+# def friends_games():
+#     return render_template('friendsgames.html')
 
-@app.route('/howitworks')
-def how_it_works():
-    return render_template('howitworks.html')
+# @app.route('/howitworks')
+# def how_it_works():
+#     return render_template('howitworks.html')
 
-#Model Definitions ===========================================================
+# #Model Definitions ===========================================================
 
-class User(db.Model):
+# class Users(db.Model):
 
-    __tablename__ = 'users'
+#     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    email = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(64), nullable=False)
-    steam_id = db.Column(db.String(100), nullable=False)
-    friend_list=db.Column(db.Integer, db.ForeignKey('friends.id'), nullable=False)
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     name = db.Column(db.String(64), nullable=False)
+#     email = db.Column(db.String(64), nullable=False)
+#     password = db.Column(db.String(64), nullable=False)
+#     steam_id = db.Column(db.String(100), nullable=False)
+#     friend_list=db.Column(db.Integer, db.ForeignKey('friends.id'), nullable=False)
 
-class Reviews(db.Model):
+#     def __init__(self, name, email):
+#         self.name = name
+#         self.email = email
 
-    __tablename__ = 'reviews'
+# class Reviews(db.Model):
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    like_dislike = db.Column(db.Boolean, nullable=False)
+#     __tablename__ = 'reviews'
 
-class Games(db.Model):
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     like_dislike = db.Column(db.Boolean, nullable=False)
 
-    __tablename__ = 'games'
+# class Games(db.Model):
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Decimal, nullable=False)
+#     __tablename__ = 'games'
 
-class Friends(db.Model):
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     price = db.Column(db.Decimal, nullable=False)
 
-    __tablename__ = 'friends'
+# class Friends(db.Model):
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    friend_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     __tablename__ = 'friends'
+
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     friend_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
-
+    # db.create_all()
     app.run(debug=True, host="0.0.0.0")
 
 
